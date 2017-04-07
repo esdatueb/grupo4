cdff1a0cd41f88f9f6e4b24a9edeb06706ed8cdf
etamot <daatorresgu@gmail.com>
2017-04-07 11:46:45 -0500

    Update SorterBigFileTemplateImpl.java
    Método mergeFiles()

--- src/main/java/co/edu/uelbosque/essatueb/sortbigfiles/SorterBigFileTemplateImpl.java
+++ src/main/java/co/edu/uelbosque/essatueb/sortbigfiles/SorterBigFileTemplateImpl.java
@@ -85,49 +85,66 @@ public class SorterBigFileTemplateImpl extends SorterBigFileTemplate{
 
     @Override
     protected File mergeFiles(File file1, File file2) {
-        File archivoSalida = new File(this.outPutDir+"/archivoSalida.txt");
-       FileWriter fw = null;
-       BufferedWriter bw = null;
-        try {
-            fw = new FileWriter(archivoSalida, true);
-            bw = new BufferedWriter(fw);
-        } catch (IOException ex) {
-            Logger.getLogger(SorterBigFileTemplateImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        try {
-            FileInputStream fis = new FileInputStream(file1);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-            String linea;
-            while((linea = br.readLine()) != null){
-                System.out.println("Linea: "+linea);
-                bw.write(linea);
-                bw.newLine();
+            File archivoSalida = new File(this.outPutDir+"/archivoSalida.txt");
+           FileWriter fw = null;
+           BufferedWriter bw = null;
+            try {
+                fw = new FileWriter(archivoSalida, true);
+                bw = new BufferedWriter(fw);
+                ArrayList<Persona> personas = new ArrayList<>();
+                FileInputStream fis = new FileInputStream(file1);
+                BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+                FileInputStream fis2 = new FileInputStream(file2);
+                BufferedReader br2 = new BufferedReader(new InputStreamReader(fis2));
+                String linea;
+                String linea2;
+                EdadComparator comparador = new EdadComparator();
+                String[] primeraPersona = br.readLine().split(",");
+                int edadPrimeraPersona = Integer.parseInt(primeraPersona[0]);
+                Persona personaMayor = new Persona(edadPrimeraPersona, primeraPersona[1], primeraPersona[2], primeraPersona[3], primeraPersona[4]);
+                while((linea = br.readLine()) != null && (linea2 = br2.readLine()) !=null){
+                    if(linea == null){
+                        bw.write(linea2);
+                         bw.newLine();
+                    }else if(linea2 == null){
+                        bw.write(linea);
+                         bw.newLine();
+                     } else{
+                        String[] arreglo;
+                        int edad;
+                        personas.add(personaMayor);
+                        //convierte a la linea del primer archivo en una persona
+                        arreglo = linea.split(",");
+                        edad = Integer.parseInt(arreglo[0]);
+                        Persona persona1 = new Persona(edad,arreglo[1],arreglo[2],arreglo[3],arreglo[4]);
+                        personas.add(persona1);
+                        //convierte a la linea del segundo archivo en otra persona
+                        arreglo = linea2.split(",");
+                        edad = Integer.parseInt(arreglo[0]);
+                        Persona persona2 = new Persona(edad,arreglo[1],arreglo[2],arreglo[3],arreglo[4]);
+                        personas.add(persona2);
+                        //compara las edades de las personas
+                        personas.sort(comparador);
+
+                        linea = personas.get(0).getEdad()+","+personas.get(0).getDocumento()+","+personas.get(0).getNombre()
+                                +","+personas.get(0).getTipoDoc()+","+personas.get(0).getTipoSangre();
+                        linea2 = personas.get(1).getEdad()+","+personas.get(1).getDocumento()+","+personas.get(1).getNombre()
+                                +","+personas.get(1).getTipoDoc()+","+personas.get(1).getTipoSangre();
+                        personaMayor = personas.get(2);
+
+                        bw.write(linea);
+                        bw.write(linea2);
+                        bw.newLine();
+                        personas.clear();
+                    }
+                }
+                bw.close();
+                fw.close();
+
+            } catch (IOException ex) {
+                Logger.getLogger(SorterBigFileTemplateImpl.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-            FileInputStream fis2 = new FileInputStream(file2);
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(fis2));
-          
-            while((linea = br2.readLine()) != null){
-                System.out.println("Linea: "+linea);
-                bw.write(linea);
-                bw.newLine();
-            }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(SorterBigFileTemplateImpl.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(SorterBigFileTemplateImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            bw.close();
-            fw.close();
-            
-        } catch (IOException ex) {
-            Logger.getLogger(SorterBigFileTemplateImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        
-        return archivoSalida;
+            return archivoSalida;
     }
 
     @Override
